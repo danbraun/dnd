9e87d1b0e185f8be847b4b1c2344811bb1396cba
Dan Braun <daniel@danbraun.me>
2019-12-12 09:16:18 -0600

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { DragDropContext } from 'react-beautiful-dnd'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const getItems = count => (Array.from({length: count}, (el, index) => index).map( index => ({
+    'id': `item-${index}`,
+    'content': `item-${index}`
+})));
+    
+
+class App extends React.Component {
+
+    
+    constructor(props) {
+        super(props);
+        console.log(getItems(10));
+    }
+
+    render() {
+        return (
+
+            <div>
+                App
+            </div>
+        )
+    }
 }
 
 export default App;
